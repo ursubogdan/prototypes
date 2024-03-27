1380f978b77458520f94c75e8970d523345a0e37
ursubogdan <bogdannursu@gmail.com>
2024-03-27 18:49:12 +0200

    refactor code from prototypes to ES6 classes

--- js/site/all.js
+++ js/site/all.js
@@ -643,47 +643,98 @@
     }
   };
 
-  function Item() {};
-  Item.prototype.type = 'floral';
-  Item.prototype.logItem = function() {
-    console.log('%c' + this.name,'font-weight: bold');
-    for (let prop in this) {
-      console.log(' ', prop, ': ', this[prop])
+  // function Item() {};
+  // Item.prototype.type = 'floral';
+  // Item.prototype.logItem = function() {
+  //   console.log('%c' + this.name,'font-weight: bold');
+  //   for (let prop in this) {
+  //     console.log(' ', prop, ': ', this[prop])
+  //   }
+  // };
+  class Item {
+    constructor() {
+      this.type = 'goods';
     }
-  };
-
-  function Live(name, pot, quantity = 1) {
-    this.name = name;
-    this.pot = pot;
-    this.quantity = quantity;
+  logItem() {
+      console.log('%c' + this.name,'font-weight: bold');
+      for (let prop in this) {
+        console.log(' ', prop, ': ', this[prop])
+      }
+    }
   }
-    Live.prototype = new Item();
-   Live.prototype.storage = 'warm';
 
-  function Flower(quantity, color) {
-    this[color] = quantity;
+  // function Live(name, pot, quantity = 1) {
+  //   this.name = name;
+  //   this.pot = pot;
+  //   this.quantity = quantity;
+  // }
+  //   Live.prototype = new Item();
+  //   Live.prototype.storage = 'warm';
+  class Live extends Item {
+    constructor(name, pot, quantity = 1) {
+      super();
+      this.name = name;
+      this.pot = pot;
+      this.quantity = quantity;
+      this.storage = 'warm';
+    }
   }
-  Flower.prototype = new Item();
 
-  function Cut() {};
-  Cut.prototype = new Item();
-  Cut.prototype.storage = 'cool';
-
-  function Arrangement(name, vase, quantity = 1) {
-    this.name = name;
-    this.vase = vase;
-    this.quantity = quantity;
+  // function Flower(quantity, color) {
+  //   this[color] = quantity;
+  // }
+  // Flower.prototype = new Item();
+  class Flower extends Item {
+    constructor(quantity, color) {
+      super();
+      this[color] = quantity;
+    }
   }
-  Arrangement.prototype = new Cut();    
-  
-    function Bouquet(name, vase) {
+
+  // function Cut() {};
+  // Cut.prototype = new Item();
+  // Cut.prototype.storage = 'cool';
+  class Cut extends Item {
+    constructor(name, vase) {
+      super();
       this.name = name;
       this.vase = vase;
+      this.storage = 'cool';
+
     }
-    Bouquet.prototype = new Cut();
-    Bouquet.prototype.flowers = {
-      addStem: function(name, quantity = 1, color = 'Default') {
-        this[name] = new Flower(quantity, color)
+  }
+
+  // function Arrangement(name, vase, quantity = 1) {
+  //   this.name = name;
+  //   this.vase = vase;
+  //   this.quantity = quantity;
+  // }
+  // Arrangement.prototype = new Cut();   
+  class Arrangement extends Cut {
+    constructor(name, vase, quantity = 1) {
+      super(name, vase);
+      this.quantity = quantity;
+    }
+  } 
+  
+    // function Bouquet(name, vase) {
+    //   this.name = name;
+    //   this.vase = vase;
+    // }
+    // Bouquet.prototype = new Cut();
+    // Bouquet.prototype.flowers = {
+    //   addStem: function(name, quantity = 1, color = 'Default') {
+    //     this[name] = new Flower(quantity, color)
+    //   }
+    // };
+    class Bouquet extends Cut{
+      constructor(name, vase) {
+        super(name, vase);
+        this.flowers = {
+          addStem: function(name, quantity = 1, color = 'Default') {
+            this[name] = new Flower(quantity, color)
+          }
+        }
       }
-    };
+    }
 })();
